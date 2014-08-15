831d75e4342fd89b20f89cdcae5441874716efa7
Jasmin Shin <jasmin_shin@Jasmins-MacBook-Pro-2.local>
2014-08-15 01:55:13 -0700

    add everything

---
+++ doxygen.sh
@@ -0,0 +1,22 @@
+#script used to update web page.
+#have Doxygen terminal commands installed.
+#for mac: sudo ln -s /Applications/Doxygen.app/Contents/Resources/doxygen /usr/local/bin
+#have git, have git hub account, ask me to add you up.    
+
+
+#!/bin/bash
+doxygen DoxySite/config3
+
+
+
+git add .
+ 
+message=0
+echo -n "message:"
+read message
+git commit -m message
+#is this going to be variable message?  
+
+git push origin
+
+git submodule update
